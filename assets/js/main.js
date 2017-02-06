b65af97acae1c30d1ff7fb4cec1a6caa5a213b24
dnialwill <dnialwill@gmail.com>
2017-02-06 18:19:22 -0500

    Added card generation

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,18 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+var cardMax = 4;
 
-if (cardTwo === cardFour){
-  alert('You found a match!');
-} else {
-  alert('Sorry, try again.');
+function createCards() {
+  for (var cardCount = 1; cardCount<=cardMax; cardCount++) {
+    var cardNew = document.createElement("div");
+    cardNew.setAttribute("class", "card");
+    document.querySelector('#game-board').appendChild(cardNew);
+  }
 }
+
+// if (cardTwo === cardFour){
+//   alert('You found a match!');
+// } else {
+//   alert('Sorry, try again.');
+// }
