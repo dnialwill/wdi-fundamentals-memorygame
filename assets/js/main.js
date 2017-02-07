438d4b0730b8ab89ca93065f173813e978db51bd
dnialwill <dnialwill@gmail.com>
2017-02-07 16:14:40 -0500

    completed basic game logic

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,28 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-var cardMax = 4;
+var cards = ["queen", "queen", "king", "king"]
+var cardsInPlay = []
 
 function createCards() {
-  for (var cardCount = 1; cardCount<=cardMax; cardCount++) {
-    var cardNew = document.createElement("div");
-    cardNew.setAttribute("class", "card");
-    document.querySelector('#game-board').appendChild(cardNew);
+  for (var i = 0; i<cards.length; i++) {
+    var cardElement = document.createElement("div");
+    cardElement.className = "card";
+    cardElement.setAttribute("data-card", cards[i]);
+    document.querySelector("#game-board").appendChild(cardElement);
+    cardElement.addEventListener("click", isTwoCards);
   }
 }
 
-// if (cardTwo === cardFour){
-//   alert('You found a match!');
-// } else {
-//   alert('Sorry, try again.');
-// }
+function isMatch(){
+  if (cardsInPlay[0] === cardsInPlay[1]){
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again.");
+  }
+}
+
+function isTwoCards() {
+  cardsInPlay.push(this.getAttribute("data-card"));
+  if (cardsInPlay.length === 2) {
+    isMatch(cardsInPlay);
+    cardsInPlay = [];
+  }
+}
