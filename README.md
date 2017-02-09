3308453fa80f5a70638db3b457d893b86536d73a
dnialwill <dnialwill@gmail.com>
2017-02-09 11:43:27 -0500

    Added card graphics, license, RM. Game logic improv.

--- README.md
+++ README.md
@@ -2,5 +2,4 @@
 
 Memory Game: Unit Homework for WDI Fundamentals
 
-In order to apply JavaScript concepts covered in the [pre-work](http://fundamentals.generalassemb.ly/), all pre-work lessons will work towards building a memory game app. Your development of this game will exercise your ability to implement logic with JavaScript and so much more! Besides following the prompts, you should feel free to be creative and add to the code - make this game your own!
-
+This is my fork of the final project for the WDI Fundamentals section of the March 2017 WDIr course. It currently meets the deliverable standards for the project but will likely be undergoing some significant revision during the coming weeks to improve and expand upon its current functionality and design. This fork utilizes resources from donpark's scalable-css-playing-cards (http://donpark.github.io/scalable-css-playing-cards/); though only the face card and card back images have been included in this repo, it is likely that the scalable-css-playing-cards software will be implemented in the future, thus the original license is appended in full to "LICENSE.md".
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,24 @@
-var cards = ["queen", "queen", "king", "king"]
+var cards = ["queen-red", "queen-black", "king-red", "king-black"]
 var cardsInPlay = []
 
 function createCards() {
   for (var i = 0; i<cards.length; i++) {
     var cardElement = document.createElement("div");
-    cardElement.className = "card";
-    cardElement.setAttribute("data-card", cards[i]);
+    cardElement.className = "card back";
+    cardElement.setAttribute("cardData", cards[i]);
+    if ((cards[i] === "queen-red") || (cards[i] === "queen-black")) {
+      cardElement.setAttribute ("cardFace", "queen");
+    }
+    else if ((cards[i] === "king-red") || (cards[i] === "king-black")) {
+      cardElement.setAttribute ("cardFace", "king");
+    }
     document.querySelector("#game-board").appendChild(cardElement);
     cardElement.addEventListener("click", isTwoCards);
   }
 }
 
-function isMatch(){
-  if (cardsInPlay[0] === cardsInPlay[1]){
+function isMatch() {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
     alert("You found a match!");
   } else {
     alert("Sorry, try again.");
@@ -20,7 +26,8 @@ function isMatch(){
 }
 
 function isTwoCards() {
-  cardsInPlay.push(this.getAttribute("data-card"));
+  this.className = `card ${this.getAttribute("cardData")}`;
+  cardsInPlay.push(this.getAttribute("cardFace"));
   if (cardsInPlay.length === 2) {
     isMatch(cardsInPlay);
     cardsInPlay = [];
